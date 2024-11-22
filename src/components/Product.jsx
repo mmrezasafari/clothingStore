c5c5deeece2a1085f54b438b923b34ff64dc2852
mmrezasafari <mmrezadev@gmail.com>
2024-11-23 01:16:44 +0330

    add :: added handleAddToCart functionality to button

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,10 @@
-export default function Product({ image, title, price, description }) {
+import { useContext } from "react"
+import { ShoppingCartContext } from "../context/ShoppingCartContext"
+
+export default function Product({ id, image, title, price, description }) {
+
+  const { handleAddToCart } = useContext(ShoppingCartContext)
+
   return (
     <article className="flex flex-col text-red-950 bg-orange-200 h-full">
       <img src={image} alt={title} />
@@ -7,7 +13,7 @@ export default function Product({ image, title, price, description }) {
         <p className="text-xl font-extrabold mb-2">${price}</p>
         <p>{description}</p>
         <div className="flex items-end justify-end h-full relative mt-5">
-          <button className="p-3 rounded bg-red-950 h-fit text-orange-200">Add to cart</button>
+          <button className="p-3 rounded bg-red-950 h-fit text-orange-200" onClick={() => handleAddToCart({ title, price, id })}>Add to cart</button>
         </div>
       </div>
     </article>
